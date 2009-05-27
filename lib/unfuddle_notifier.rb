a246ebf0c8c93c2e021f49f3ccad9e211cfaf1c7
timmatheson <me@timmatheson.com>
2009-05-27 08:59:46 -0700

    Trying to render out a string without ERB

--- lib/unfuddle_notifier.rb
+++ lib/unfuddle_notifier.rb
@@ -6,6 +6,6 @@ class UnfuddleNotifier
   
   def call(env)
     status, headers, response = @app.call(env)
-    [status, headers, File.read_inline("../views/ui/panel.html.erb") + response.body]
+    [status, headers, File.readlines("../views/ui/panel.html.erb") + response.body]
   end
 end
