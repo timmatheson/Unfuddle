bd685a073de3bb56e4d9d20a3c3599b1f2e36ccf
timmatheson <me@timmatheson.com>
2009-05-27 16:47:03 -0700

    fixing install

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
-#rake_task_copy_path = File.join(RAILS_ROOT, "lib", "tasks", "unfuddle.rake")
-#unless File.exists?(rake_task_copy_path)
-#  File.copy(File.join(File.dirname(__FILE__),"lib","unfuddke.rake"), rake_task_copy_path)
-#end
+rake_task_copy_path = File.join(RAILS_ROOT, "lib", "tasks", "unfuddle.rake")
+unless File.exists?(rake_task_copy_path)
+  File.copy(File.join(File.dirname(__FILE__),"lib","unfuddke.rake"), rake_task_copy_path)
+end
