31d57ba2aeb70a1b41503c121db505141f31a808
timmatheson <me@timmatheson.com>
2009-05-27 02:26:33 -0700

    Adding action view to the rack app

--- lib/unfuddle_notifier.rb
+++ lib/unfuddle_notifier.rb
@@ -1,14 +1,12 @@
 class UnfuddleNotifier
+  require 'action_view'
+  
   def initialize(app)
     @app = app
   end
   
   def call(env)
-    @session = Unfuddle::Session.new
-    @project = Unfuddle::Project.find(:one, :from => "/projects/20935")
-    @tickets = @project.tickets
     status, headers, response = @app.call(env)
-    [status, headers, "<div style='background-color: blue; color: white; font-weight: bold;'>
-      #{@project.title} has #{@tickets.size} tickets</div>" + response.body]
+    [status, headers, ActionView::Template.render("panel", "views") + response.body]
   end
 end
